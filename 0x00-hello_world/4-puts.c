6a3a9a0f4b081a6f672b6d084c073d671b44d886
olayemmy <temikayluv@gmail.com>
2022-06-02 10:24:08 -0700

    Corrected the Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 /**
- *
  *  main - This is the Entry point
- *
  *
  *  Return: Always 0 which means (Success)
  */
-
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
