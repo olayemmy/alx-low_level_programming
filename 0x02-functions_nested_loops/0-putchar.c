133b9916defffbc74e8fbff81b618fc902ddc158
olayemmy <temikayluv@gmail.com>
2022-06-08 11:54:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "school.h"
+
+/**
+ * main - Print "school"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+		int i = 0;
+
+			char name_of_school[] = "school";
+
+
+
+				while (i < 9)
+				{
+					_putchar(name_of_school[i]);
+					i++;
+				}
+				_putchar('\n');
+
+		return (0);
+
+}
