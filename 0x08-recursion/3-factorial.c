ee613a33b870f7f7ca2cc5d7dfcda8d2060efe13
olayemmy <temikayluv@gmail.com>
2022-06-23 09:58:32 -0700

    changes made to task 2 and 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - main - check the code
+ * Description: factorial code
+ * @n: number of factorial
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
